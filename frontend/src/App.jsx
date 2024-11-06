16ad530aa41f0d7b83dda9e1f7f88f202ca3c9bd
Coffemug209 <irhamthoriqsahsecaatmaja1@gmail.com>
2024-11-06 16:53:29 +0700

    new navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,19 @@ import PageContact from './pages/PageContact';
 const App = () => {
     return (
         <Router>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/voting">Voting</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
+            <nav class="bg-gray-800 shadow-lg p-4">
+                <div class="container mx-auto px-4">
+                    <ul class="flex space-x-4 pl-auto">
+                        <li><Link to="/" class="text-gray-100 hover:text-gray-300">Home</Link></li>
+                        <li><Link to="/about" class="text-gray-100 hover:text-gray-300">About</Link></li>
+                        <li><Link to="/voting" class="text-gray-100 hover:text-gray-300">Voting</Link></li>
+                        <li><Link to="/contact" class="text-gray-100 hover:text-gray-300">Contact</Link></li>
+                    </ul>
+                </div>
             </nav>
+
+
+
             <Routes>
                 <Route path="/" element={<PageHome />} />
                 <Route path="/about" element={<PageAbout />} />
@@ -23,6 +28,7 @@ const App = () => {
                 <Route path="/contact" element={<PageContact />} />
             </Routes>
         </Router>
+
     );
 };
 
