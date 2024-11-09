1c644a2bda3195f8bc6e897210851429bf5530c5
basyaralyassar@gmail.com <basyaralyassar@gamil.com>
2024-11-09 15:00:52 +0700

    update

--- frontend/src/pages/PageContact.jsx
+++ frontend/src/pages/PageContact.jsx
@@ -10,27 +10,27 @@ const PageContact = () => {
     emailjs
       .sendForm('service_amj4b18', 'template_thzok5u', form.current, 'uj9vf0szqjqzPche7')
       .then(() => {
-        alert('Terima kasih! Pesan Anda telah dikirim.');
+        alert('Thank you! Your message has been sent.');
         form.current.reset();
       })
       .catch((error) => {
-        alert('Maaf, terjadi kesalahan. Silakan coba lagi.');
+        alert('Sorry, an error occurred. Please try again.');
         console.error('Error:', error);
       });
   };
 
   return (
-    <section className="py-10 px-4 md:px-8 lg:px-20 bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-6">
-        <span className="text-blue-500">Contact</span>
+    <section className="py-10 px-4 sm:px-6 md:px-8 lg:px-20 bg-gray-900 text-white">
+      <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center md:text-left">
+        <span className="text-blue-500">Contact</span> Us
       </h1>
-      <div className="flex flex-col md:flex-row md:space-x-10 space-y-10 md:space-y-0">
-        <div className="bg-gray-800 p-8 rounded-lg shadow-md mb-10 md:mb-0">
+      <div className="flex flex-col lg:flex-row lg:space-x-10 space-y-10 lg:space-y-0">
+        <div className="bg-gray-800 p-6 md:p-8 rounded-lg shadow-md w-full lg:w-1/2">
           <h2 className="text-xl font-semibold mb-4">Contact Us</h2>
           <p className="text-white mb-6">For more information, you can contact us:</p>
           <form ref={form} onSubmit={handleSubmit} className="space-y-6">
             <div className="mb-4">
-              <label className="block text-white mb-1">Your Name</label>
+              <label className="block text-white mb-1">Name</label>
               <input
                 type="text"
                 name="user_name"
@@ -40,7 +40,7 @@ const PageContact = () => {
               />
             </div>
             <div className="mb-4">
-              <label className="block text-white mb-1">Your Email</label>
+              <label className="block text-white mb-1">Email</label>
               <input
                 type="email"
                 name="user_email"
@@ -53,7 +53,7 @@ const PageContact = () => {
               <label className="block text-white mb-1">Message</label>
               <textarea
                 name="message"
-                placeholder="Type Message"
+                placeholder="Type Your Message"
                 className="text-black w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 required
               />
@@ -67,17 +67,16 @@ const PageContact = () => {
           </form>
         </div>
 
-        <div className="bg-gray-800 p-8 rounded-lg shadow-md">
+        <div className="bg-gray-800 p-6 md:p-8 rounded-lg shadow-md w-full lg:w-1/2">
           <h2 className="text-xl font-semibold mb-4">Meet Us At</h2>
           <p className="text-white mb-2">
-            Jl. Dr. KRT Radjiman Widyodiningrat No.32, RT.07/RW.7, Rawa Badung, Kec. Cakung, Kota Jakarta Timur, Daerah
-            Khusus Ibukota Jakarta 13930
+          Jl. Dr. KRT Radjiman Widyodiningrat No.32, RT.07/RW.7, Rawa Badung, Kec. Cakung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13930
           </p>
-          <p className="text-white mb-6">No. Telepon: 0813-8877-2645</p>
+          <p className="text-white mb-6">Phone Number: 0813-8877-2645</p>
           <div className="map mb-4">
             <iframe
               title="Map Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4342287584636!2d106.92306417402486!3d-6.206312860792983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698bcabb1368d7%3A0xea46dd080cc5e54c!2sSMK%20NEGERI%2069%20JAKARTA!5e0!3m2!1sid!2sid!4v1730899643322!5m2!1sid!2sid"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.4342287584636!2d106.92306417402486!3d-6.206312860792983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698bcabb1368d7%3A0xea46dd080cc5e54c!2sSMK%20NEGERI%2069%20JAKARTA!5e0!3m2!1sen!2sid!4v1730899643322!5m2!1sen!2sid"
               className="w-full h-48 md:h-64 lg:h-72 border-0 rounded-md"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
